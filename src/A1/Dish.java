d4efd2c729072c9b9bb194a4a269809b8de6b12e
JieHuang <1774046660@qq.com>
2022-09-17 23:08:00 +0800

    项目初始化

---
+++ src/A1/Dish.java
@@ -0,0 +1,17 @@
+package A1;
+
+public class Dish {
+    //编号
+    int id;
+    //菜的名字
+    String name;
+    //菜的价格
+    double price;
+
+    //构造方法 alt+insert
+    public Dish(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
